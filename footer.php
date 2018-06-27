5d0fa4914061e3c6dc2d897f3f1de5d476a45191
lucsfernandes <lbandeira845@gmail.com>
2018-06-27 09:24:04 -0300

    Arquivos iniciais para a criação do Template

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+  <p>
+    Rodapé
+  </p>
+</footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+    <div class="conteudo">
+      <main>
+        <section class="slide">Slide</section>
+        <section class="servicos">Serviços</section>
+        <section class="meio">
+          <aside class="barra-lateral">Barra Lateral</aside>
+          <div class="noticias">Notícias</div>
+        </section>
+      <section class="mapa">Mapa</section>
+      </main>
+    </div>
+
+  <?php get_footer(); ?>
